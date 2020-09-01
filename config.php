1c9e6835b737a3ce136401f8b160edb36d794d23
hootrix <hhtjim@gmail.com>
2020-09-01 23:48:59 +0800

    require config

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+return [
+//    '/test/aa' => 'http://mock-api.com/VnZ8Eqzw.mock/boluo',
+];
--- main.php
+++ main.php
@@ -7,16 +7,18 @@ $map = [
     //请求访问的uri路径  =>  需要返回的网址内容
     '/test' => 'http://mock-api.com/VnZ8Eqzw.mock/boluo',
 ];
+$more_config = require_once('./config.php');
+$config = array_merge($map,$more_config);
 function curl($url)
 {
     return file_get_contents($url);
 }
 if (isset($_GET['s']) && !empty($_GET['s'])) {
     $req = $_GET['s'];// 当前请求的路径uri
-    if (isset($map[$req])) {
-        echo curl($map[$req]);die;
+    if (isset($config[$req])) {
+        echo curl($config[$req]);die;
     }
 }
 echo json_encode([
-    'States'=>-1,
+    'States'=>"-1", # 默认报错
 ]);
